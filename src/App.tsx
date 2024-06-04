5b895eb2b32cc8342a1a29ccad6b01ab145c41c2
BuiDay <buivanduynhat@gmail.com>
2024-06-04 14:01:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Layout from './Layout'
 
 function App() {
 
   return (
     <div className='App'>
       <Routes>
+      <Route path="/" element={<Layout />}>
         <Route path='/' element={<Home />} />
+        </Route>
       </Routes>
     </div>
   )
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Common/Header/Header';
 import { Outlet } from 'react-router-dom'
+import SidebarProfile from './Common/SidebarProfile/SidebarProfile';
 
 const Layout = () => {
     return (
         <div className='min-h-screen h-full'>
             <Header />
-            <div className='flex h-screen overflow-hidden z-4'>
+            <div className='flex h-screen overflow-hidden z-4 flex-row-reverse'>
+              <SidebarProfile />  
               <main className='w-full pt-16 bg-pri'>
                 <Outlet />
               </main>
